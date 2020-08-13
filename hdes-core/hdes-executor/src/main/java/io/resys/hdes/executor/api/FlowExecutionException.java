1eac2c875bc1dfacb01ad91536c46fff726bbad2
Olev Mutso <olev.mutso@resys.io>
2020-08-13 14:34:35 +0300

    flow

---
+++ hdes-core/hdes-executor/src/main/java/io/resys/hdes/executor/api/FlowExecutionException.java
@@ -0,0 +1,24 @@
+package io.resys.hdes.executor.api;
+
+import io.resys.hdes.executor.api.FlowMeta.FlowState;
+
+public class FlowExecutionException extends RuntimeException {
+  
+  private static final long serialVersionUID = 2509713376857138221L;
+  private final String msg;
+  private final FlowState state;
+  
+  public FlowExecutionException(FlowState state, String msg) {
+    super(msg);
+    this.msg = msg;
+    this.state = state;
+  }
+
+  public String getMsg() {
+    return msg;
+  }
+
+  public FlowState getState() {
+    return state;
+  }
+}
